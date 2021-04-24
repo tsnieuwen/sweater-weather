459a01a366353e612616164d2ee67ea10e1dbd95
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-24 14:17:56 -0600

    Complete forecast feature, need to revisit and beef up testing

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    forecasts = ForecastFacade.return_forecasts(params[:location])
+    @forecasts = ForecastFacade.return_forecasts(params[:location])
+    @serial = ForecastSerializer.new(@forecasts)
+    render json: @serial
   end
 
 end
--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,54 +2,98 @@ class ForecastFacade
 
   def self.return_forecasts(location)
     body = ForecastService.coordinate_digest(location)
-    OpenStruct.new({
-      forecast: self.current_weather(body),
+    o = OpenStruct.new({
+      id: nil,
+      current_weather: self.current_weather(body),
       daily_weather: self.daily_weather(body),
       hourly_weather: self.hourly_weather(body)
       })
   end
 
+  # def self.current_weather(body)
+  #   current_weather = body[:current]
+  #   OpenStruct.new({
+  #     datetime: Time.at(current_weather[:dt]).to_s,
+  #     sunrise: Time.at(current_weather[:sunrise]).to_s,
+  #     sunset: Time.at(current_weather[:sunset]).to_s,
+  #     temperature: (1.8 * ((current_weather[:temp]) - 273.15) + 32).round(2),
+  #     feels_like: (1.8 * ((current_weather[:feels_like]) - 273.15) + 32).round(2),
+  #     humidity: current_weather[:humidity],
+  #     uvi: current_weather[:uvi],
+  #     visibility: current_weather[:visibility],
+  #     conditions: current_weather[:weather][0][:description],
+  #     icon: current_weather[:weather][0][:icon]
+  #     })
+  # end
+
   def self.current_weather(body)
-    current_weather = body[:current]
-    OpenStruct.new({
-      datetime: Time.at(current_weather[:dt]).to_s,
-      sunrise: Time.at(current_weather[:sunrise]).to_s,
-      sunset: Time.at(current_weather[:sunset]).to_s,
-      temperature: (1.8 * ((current_weather[:temp]) - 273.15) + 32).round(2),
-      feels_like: (1.8 * ((current_weather[:feels_like]) - 273.15) + 32).round(2),
-      humidity: current_weather[:humidity],
-      uvi: current_weather[:uvi],
-      visibility: current_weather[:visibility],
-      conditions: current_weather[:weather][0][:description],
-      icon: current_weather[:weather][0][:icon]
-      })
+    weather_now = body[:current]
+    current_weather_hash = Hash.new
+    current_weather_hash[:datetime] = Time.at(weather_now[:dt]).to_s
+    current_weather_hash[:sunrise] = Time.at(weather_now[:sunrise]).to_s
+    current_weather_hash[:sunset] = Time.at(weather_now[:sunset]).to_s
+    current_weather_hash[:temperature] = (1.8 * ((weather_now[:temp]) - 273.15) + 32).round(2)
+    current_weather_hash[:feels_like] = (1.8 * ((weather_now[:feels_like]) - 273.15) + 32).round(2)
+    current_weather_hash[:humidity] = weather_now[:humidity]
+    current_weather_hash[:uvi] = weather_now[:uvi]
+    current_weather_hash[:visibility] = weather_now[:visibility]
+    current_weather_hash[:conditions] = weather_now[:weather][0][:description]
+    current_weather_hash[:icon] = weather_now[:weather][0][:icon]
+    current_weather_hash
   end
 
   def self.daily_weather(body)
     daily_weather = body[:daily][1..5]
     array = daily_weather.map do |day_weather|
-      OpenStruct.new({
-        date: Time.at(day_weather[:dt]).strftime('%F'),
-        sunrise: Time.at(day_weather[:sunrise]).to_s,
-        sunset: Time.at(day_weather[:sunset]).to_s,
-        max_temp: (1.8 * ((day_weather[:temp][:max]) - 273.15) + 32).round(2),
-        min_temp: (1.8 * ((day_weather[:temp][:min]) - 273.15) + 32).round(2),
-        conditions: day_weather[:weather][0][:description],
-        icon: day_weather[:weather][0][:icon]
-        })
+      daily_weather_hash = Hash.new
+        daily_weather_hash[:date] = Time.at(day_weather[:dt]).strftime('%F')
+        daily_weather_hash[:sunrise] = Time.at(day_weather[:sunrise]).to_s
+        daily_weather_hash[:sunset] = Time.at(day_weather[:sunset]).to_s
+        daily_weather_hash[:max_temp] = (1.8 * ((day_weather[:temp][:max]) - 273.15) + 32).round(2)
+        daily_weather_hash[:min_temp] = (1.8 * ((day_weather[:temp][:min]) - 273.15) + 32).round(2)
+        daily_weather_hash[:conditions] = day_weather[:weather][0][:description]
+        daily_weather_hash[:icon] = day_weather[:weather][0][:icon]
+      daily_weather_hash
     end
   end
 
   def self.hourly_weather(body)
     hourly_weather = body[:hourly][1..8]
     array = hourly_weather.map do |hour_weather|
-      OpenStruct.new({
-        time: Time.at(hour_weather[:dt]).strftime('%T'),
-        temperature: (1.8 * ((hour_weather[:temp]) - 273.15) + 32).round(2),
-        conditions: hour_weather[:weather][0][:description],
-        icon: hour_weather[:weather][0][:icon]
-        })
+      hourly_weather_hash = Hash.new
+        hourly_weather_hash[:time] = Time.at(hour_weather[:dt]).strftime('%T')
+        hourly_weather_hash[:temperature] =  (1.8 * ((hour_weather[:temp]) - 273.15) + 32).round(2)
+        hourly_weather_hash[:conditions] = hour_weather[:weather][0][:description]
+        hourly_weather_hash[:icon] = hour_weather[:weather][0][:icon]
+      hourly_weather_hash
     end
   end
+
+  # def self.daily_weather(body)
+  #   daily_weather = body[:daily][1..5]
+  #   array = daily_weather.map do |day_weather|
+  #     OpenStruct.new({
+  #       date: Time.at(day_weather[:dt]).strftime('%F'),
+  #       sunrise: Time.at(day_weather[:sunrise]).to_s,
+  #       sunset: Time.at(day_weather[:sunset]).to_s,
+  #       max_temp: (1.8 * ((day_weather[:temp][:max]) - 273.15) + 32).round(2),
+  #       min_temp: (1.8 * ((day_weather[:temp][:min]) - 273.15) + 32).round(2),
+  #       conditions: day_weather[:weather][0][:description],
+  #       icon: day_weather[:weather][0][:icon]
+  #       })
+  #   end
+  # end
+  #
+  # def self.hourly_weather(body)
+  #   hourly_weather = body[:hourly][1..8]
+  #   array = hourly_weather.map do |hour_weather|
+  #     OpenStruct.new({
+  #       time: Time.at(hour_weather[:dt]).strftime('%T'),
+  #       temperature: (1.8 * ((hour_weather[:temp]) - 273.15) + 32).round(2),
+  #       conditions: hour_weather[:weather][0][:description],
+  #       icon: hour_weather[:weather][0][:icon]
+  #       })
+  #   end
+  # end
 
 end
