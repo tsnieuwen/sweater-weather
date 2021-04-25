ae4f122e40b6876a56a496b10b93865621de1fa1
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-24 22:12:32 -0600

    Complete happy path testing for forecast and image

--- spec/requests/api/v1/image_spec.rb
+++ spec/requests/api/v1/image_spec.rb
@@ -3,8 +3,22 @@ require 'rails_helper'
 describe "forecast return happy path" do
 
   it "returns forecast data per spec " do
+    VCR.use_cassette('taunton_image') do
       get '/api/v1/image', params: {location: "taunton,ma" }
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
       expect(response).to be_successful
+      expect(body[:data][:attributes]).to have_key(:image)
+      expect(body[:data][:attributes][:image]).to be_a(Hash)
+      expect(body[:data][:attributes][:image].keys).to eq([:location, :image_url, :credit])
+      expect(body[:data][:attributes][:image][:location]).to be_a(String)
+      expect(body[:data][:attributes][:image][:image_url]).to be_a(String)
+      expect(body[:data][:attributes][:image][:credit]).to be_a(Hash)
+      expect(body[:data][:attributes][:image][:credit].keys).to eq([:source, :author])
+      expect(body[:data][:attributes][:image][:credit][:source]).to be_a(String)
+      expect(body[:data][:attributes][:image][:credit][:author]).to be_a(String)
+    end
   end
 
 end
