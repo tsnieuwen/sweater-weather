7289a29f03d16cfbe389641e9872753b6a513599
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-27 18:51:29 -0600

    Image request at 100%

--- spec/requests/api/v1/image_spec.rb
+++ spec/requests/api/v1/image_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe "forecast return happy path" do
+describe "image return happy paths" do
 
-  it "returns forecast data per spec " do
+  it "returns image data per spec" do
     VCR.use_cassette('taunton_image') do
       get '/api/v1/image', params: {location: "taunton,ma" }
 
@@ -18,6 +18,25 @@ describe "forecast return happy path" do
       expect(body[:data][:attributes][:image][:credit].keys).to eq([:source, :author])
       expect(body[:data][:attributes][:image][:credit][:source]).to be_a(String)
       expect(body[:data][:attributes][:image][:credit][:author]).to be_a(String)
+    end
+  end
+
+end
+
+describe "image return sad paths" do
+
+  it "returns if image search is blank" do
+    VCR.use_cassette('taunton_image') do
+      get '/api/v1/image', params: {location: "" }
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+
+      expect(body.keys).to eq([:data])
+      expect(body[:data]).to be_a(Hash)
+      expect(body[:data].keys).to eq([:error])
+      expect(body[:data][:error]).to eq("Please enter a valid location")
     end
   end
 
